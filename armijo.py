390447167115fd0b7d437e791119c45ee74a9322
Naveen Joseph Delphin <naveenjosephdelphin@gmail.com>
2025-01-31 14:02:19 +0100

    added armijo changes from yesterday

--- armijo.py
+++ armijo.py
@@ -2,163 +2,100 @@ import numpy as np
 import matplotlib.pyplot as plt
 from cost_fn import cost
 from flexible_dyn import x_next_lambda as dyn_lambda
- 
- 
-def select_stepsize(stepsize_0, armijo_maxiters, cc, beta, deltau, 
-                    xx_ref, uu_ref,  x0, uu, JJ, descent_arm, plot = True):
- 
-      """
-      Computes the stepsize using Armijo's rule.
-      input parameters:
-            - stepsize_0 : initial stepsize guess,
-            - armijo_maxiters : maximum number of iterations for armijo rule,
-            - deltau : descending direction for the control action,
-            - xx_ref : reference trajectory state,
-            - uu_ref : reference trajectory input,
-            - x0 : initial state,
-            - uu : input at current iteration,
-            - JJ : cost at current iteration,
-            - descent_arm: armijo descent direction at current iteration,
-            - plot: whether or not to show descent plot.
- 
-      output parameters:
-            - stepsize
-      """
- 
-      TT = uu.shape[1]
-      print('tt:',TT)
- 
-      stepsizes = []  # list of stepsizes
-      costs_armijo = []
- 
-      stepsize = stepsize_0
- 
-      ns = xx_ref.shape[0]
-      ni = uu_ref.shape[0]
-      Qt = 10*np.eye(ns)
- 
-      Rt = 1e-3*np.ones((ni, ni)) + 1e-4*np.eye(ni)
-      QT = 10*np.eye(ns)
-      x_next = dyn_lambda()
- 
-      for ii in range(armijo_maxiters):
- 
-            # temp solution update
- 
+
+def select_stepsize(stepsize_0, armijo_maxiters, cc, beta, deltau, gradJ,
+    xx_ref, uu_ref,  xx, uu, JJ, K_star, sigma_star, iteration, plot = False):
+
+    TT = uu.shape[1]
+
+    stepsizes = []  # list of stepsizes
+    costs_armijo = []
+
+    stepsize = stepsize_0
+    ns = xx_ref.shape[0]
+    ni = uu_ref.shape[0]
+    Qt = 10*np.eye(ns)
+
+    Rt = 1e-3*np.ones((ni, ni)) + 1e-4*np.eye(ni)
+    QT = 10*np.eye(ns)
+    x_next = dyn_lambda()
+
+    descent = 0
+    print('grad j 1', gradJ[:,1] )
+    print('del u 1', deltau[:,1] )
+    for t in range(TT-1):
+        descent += np.dot(gradJ[:,t] , deltau[:,t])
+        # descent += (gradJ[:,t].T @ deltau[:,t]).flatten()
+
+    print("altmethod:", np.sum(gradJ[:,:-1] * deltau))
+    print("descent:", descent)
+    for ii in range(armijo_maxiters):
+
+        xx_temp = np.zeros((ns,TT))
+        uu_temp = np.zeros((ni,TT))
+
+        xx_temp[:,0] = xx[:, 0]
+
+        for tt in range(TT-1):                  
+            uu_temp[:,tt] = uu[:,tt] + stepsize*(K_star[:,:,tt] @ (xx_temp[:,tt] - xx[:,tt]) + sigma_star[:,tt])
+            xx_temp[:,tt+1] = x_next(xx_temp[:,tt], uu_temp[:,tt]).flatten()
+
+        J_temp = cost(xx_temp, uu_temp, xx_ref, uu_ref,Qt, Rt, QT)
+        stepsizes.append(stepsize)      # save the stepsize
+        costs_armijo.append(J_temp)    # save the cost associated to the stepsize
+        print('armijo jj:',J_temp)
+        print('armijo scale factor:', JJ  + cc*stepsize*descent)
+        if J_temp > JJ  + cc*stepsize*descent:
+            # update the stepsize
+            stepsize = beta*stepsize
+        else:
+            print('Armijo stepsize = {:.3e}'.format(stepsize))
+            break
+
+        if ii == armijo_maxiters -1:
+            print("WARNING: no stepsize was found with armijo rule!")
+            stepsize = 1
+
+
+    if plot:
+
+        steps = np.linspace(0,stepsize_0,int(2e1))
+        costs = np.zeros(len(steps))
+
+        for ii in range(len(steps)):
+
+            step = steps[ii]
+
             xx_temp = np.zeros((ns,TT))
             uu_temp = np.zeros((ni,TT))
- 
-            xx_temp[:,0] = x0
- 
- 
+
+            xx_temp[:,0] = xx[:,0]
+
             for tt in range(TT-1):
-                  uu_temp[:,tt] = uu[:,tt] + stepsize*deltau[:,tt]
-                  xx_temp[:,tt+1] = x_next(xx_temp[:,tt], uu_temp[:,tt]).flatten()
- 
-            # # temp cost calculation
-            # JJ_temp = 0
- 
-            # JJ_temp = cost(xx_temp[:, ii], uu_temp[:, ii], xx_ref, uu_ref, Qt, Rt, QT)
-            print("uu_temp:",uu_temp[:, ii])
-            print("xx_temp:",xx_temp[:, ii])
-            J=0
-            T = xx_temp.shape[1]
-            for t in range(TT-2):
-                J = J + stage_cost(xx_temp[:, t], xx_ref[:, t], uu_temp[:, t], uu_ref[:, t], Qt, Rt)
+                uu_temp[:,tt] = uu[:,tt] + step*(K_star[:,:,tt] @ (xx_temp[:,tt] - xx[:,tt]) + sigma_star[:,tt])
+                xx_temp[:,tt+1] = x_next(xx_temp[:,tt], uu_temp[:,tt]).flatten()
 
-            J = J + terminal_cost(xx_temp[:, TT-1], xx_ref[:, TT-1], QT)
- 
-            stepsizes.append(stepsize)      # save the stepsize
-            costs_armijo.append(np.min([J, 100*JJ]))    # save the cost associated to the stepsize
-            print('armijo jj:',J)
-            print('armijo scale factor:', JJ  + cc*stepsize*descent_arm)
-            if J > JJ  + cc*stepsize*descent_arm:
-                  # update the stepsize
-                  stepsize = beta*stepsize
- 
-            else:
-                  print('Armijo stepsize = {:.3e}'.format(stepsize))
-                  break
-           
-            if ii == armijo_maxiters -1:
-                  print("WARNING: no stepsize was found with armijo rule!")
-           
-           
-      ############################
-      # Descent Plot
-      ############################
- 
-      if plot:
- 
-            steps = np.linspace(0,stepsize_0,int(2e1))
-            costs = np.zeros(len(steps))
- 
-            for ii in range(len(steps)):
- 
-                  step = steps[ii]
- 
-                  # temp solution update
- 
-                  xx_temp = np.zeros((ns,TT))
-                  uu_temp = np.zeros((ni,TT))
- 
-                  xx_temp[:,0] = x0
- 
-                  for tt in range(TT-1):
-                        uu_temp[:,tt] = uu[:,tt] + step*deltau[:,tt]
-                        xx_temp[:,tt+1] = x_next(xx_temp[:,tt], uu_temp[:,tt]).flatten()
- 
-                        # temp cost calculation
-                #   JJ_temp = 0
-                #   JJ_temp = cost(xx_temp[:, ii,:], uu_temp[:, ii,:], xx_ref, uu_ref, Qt, Rt, QT)
-                  J=0
-                  T = xx_temp.shape[1]
-                  for t in range(TT-2):
-                    J = J + stage_cost(xx_temp[:, t], xx_ref[:, t], uu_temp[:, t], uu_ref[:, t], Qt, Rt)
+            JJ_temp = cost(xx_temp, uu_temp, xx_ref, uu_ref,Qt, Rt, QT)
+            costs[ii] = JJ_temp
+            # costs[ii] = np.min([JJ_temp.item(), 100 * JJ])
 
-                  J = J + terminal_cost(xx_temp[:, TT-1], xx_ref[:, TT-1], QT)
- 
-                  #temp_cost = cst.termcost(xx_temp[:,-1], xx_ref[:,-1])[0]
-                  #JJ_temp += temp_cost
- 
-                  costs[ii] = np.min([J, 100*JJ])
- 
- 
-            plt.figure(1)
-            plt.clf()
- 
-     
-            plt.plot(steps, costs, color='g', label='$J(\\mathbf{u}^k - stepsize*d^k)$')
-            plt.plot(steps, JJ + descent_arm*steps, color='r', label='$J(\\mathbf{u}^k) - stepsize*\\nabla J(\\mathbf{u}^k)^{\\top} d^k$')
-            # plt.plot(steps, JJ - descent*steps, color='r', label='$J(\\mathbf{u}^k) - stepsize*\\nabla J(\\mathbf{u}^k)^{\\top} d^k$')
-            plt.plot(steps, JJ + cc*descent_arm*steps, color='g', linestyle='dashed', label='$J(\\mathbf{u}^k) - stepsize*c*\\nabla J(\\mathbf{u}^k)^{\\top} d^k$')
- 
-            plt.scatter(stepsizes, costs_armijo, marker='*') # plot the tested stepsize
- 
-            plt.grid()
-            plt.xlabel('stepsize')
-            plt.legend()
-            plt.draw()
- 
-            plt.show()
- 
-      return stepsize
 
-def stage_cost(x_stage, x_ref, u_stage, u_ref, Qt, Rt):
-    delta_x = x_stage - x_ref
-    delta_u = u_stage - u_ref
+        plt.figure(1)
+        plt.clf()
 
-    qt = (Qt @ (x_stage - x_ref)).reshape(-1, 1)
-    rt = (Rt @ (u_stage - u_ref)).reshape(-1, 1)
 
-    J_t = qt.T @ delta_x + rt.T @ delta_u + 0.5 * delta_x.T @ Qt @ delta_x + 0.5 * delta_u.T @ Rt @ delta_u
-    #print("J_t: ", J_t)
-    return J_t[-1]
+        plt.plot(steps, costs, color='g', label='$J(\\mathbf{u}^k - stepsize*d^k)$')
+        plt.plot(steps, JJ + descent*steps, color='r', label='$J(\\mathbf{u}^k) - stepsize*\\nabla J(\\mathbf{u}^k)^{\\top} d^k$')
+        plt.plot(steps, JJ + cc*descent*steps, color='g', linestyle='dashed', label='$J(\\mathbf{u}^k) - stepsize*c*\\nabla J(\\mathbf{u}^k)^{\\top} d^k$')
 
-def terminal_cost(x_term, x_ref, QT):
-    delta_x = x_term - x_ref
-    qT = (QT@(x_term-x_ref)).reshape(-1, 1)
-    J_T = qT.T @ delta_x + 0.5 * delta_x.T @ QT @ delta_x
-    #print("J_T: ", J_T)
-    return J_T[-1]
- 
+        plt.scatter(stepsizes, costs_armijo, marker='*') # plot the tested stepsize
+
+        plt.grid()
+        plt.xlabel('stepsize')
+        plt.legend()
+        plt.draw()
+
+        plt.show()
+
+
+    return stepsize
--- optimal_controller.py
+++ optimal_controller.py
@@ -38,9 +38,9 @@ def newton_optimal_control(x_ref, u_ref, timesteps=100, task=1, armijo_solver=Tr
     grad_x = grad_xu_lambda()[0]
     grad_u = grad_xu_lambda()[1]
 
-    lmbd = np.zeros((ns, TT, max_iter))  #lambdas - costate seq.
+    lmbd = np.zeros((ns, TT))  #lambdas - costate seq.
     deltau = np.zeros((nu, TT, max_iter)) #Du - descent direction
-    dJ = np.zeros((nu, TT, max_iter))     #DJ - gradient of J wrt u
+    dJ = np.zeros((nu, TT))     #DJ - gradient of J wrt u
 #    deltau_temp = np.zeros((nu, TT, max_iter))
     JJ = np.zeros(max_iter)          #collect cost
     descent = np.zeros(max_iter)     #collect descent direction
@@ -81,35 +81,23 @@ def newton_optimal_control(x_ref, u_ref, timesteps=100, task=1, armijo_solver=Tr
             A[:,:,t] = grad_x(x_opt[:,t,k], u_opt[:,t,k])
             B[:,:,t] = grad_u(x_opt[:,t,k], u_opt[:,t,k])
 
+        ########## Solve the costate equation [S20C5]
+            # Compute the effects of the inputs evolution on cost (rt)
+            # and on dynamics (B*Lambda)
+            #lmbd_temp = (QT @ (x_opt[:,-1,k] - x_ref[:,-1]))
+        lmbd[:,-1] = QT @ (x_opt[:,-1,k] - x_ref[:,-1])
+        for t in reversed(range(TT-1)):
+            rt = (Rt @ (u_opt[:,t,k] - u_ref[:,t]))
+            qt = (Qt @ (x_opt[:,t,k] - x_ref[:,t]))
+            lmbd[:, t] = A[:,:,t].T @ lmbd[:,t+1] + qt
+            dJ[:,t] = B[:,:,t].T @ lmbd[:,t+1] + rt
+        # print(dJ[:,:,1])
         K_star[:,:,:,k], sigma_star[:,:,k], del_u[:,:,k] = affine_lqr(x_opt[:,:,k], x_ref, u_opt[:,:,k], u_ref, A, B, Qt, Rt, St, QT)
         # Compute the step size
         if armijo_solver==True and k>0:
             print("TODO")
-
-            ########## Solve the costate equation [S20C5]
-            # Compute the effects of the inputs evolution on cost (rt)
-            # and on dynamics (B*Lambda)
-            lmbd_temp = (QT @ (x_opt[:,-1,k] - x_ref[:,-1]))
-            lmbd[:,t-1,k] = lmbd_temp.copy().squeeze()
-            for t in reversed(range(t-1)):
-                rt = (Rt @ (u_opt[:,t,k] - u_ref[:,t]))
-                qt = (Qt @ (x_opt[:,t,k] - x_ref[:,t]))
-                lmbd_temp = A[:,:,t].T @ lmbd[:,t+1,k] + qt
-                dJ_temp = B[:,:,t].T @ lmbd[:,t+1,k] + rt
-
-                deltau_temp = - dJ_temp
- 
-                lmbd[:,t,k] = lmbd_temp.squeeze()
-                dJ[:,t,k] = dJ_temp.squeeze()
-                deltau[:,t,k] = deltau_temp.squeeze()
- 
-                descent[k] += deltau[:,t,k].T@deltau[:,t,k]
-                descent_arm[k] += dJ[:,t,k].T@deltau[:,t,k]
-            
-
-            gamma = select_stepsize( 1,20,  0.5,0.7,
-                                deltau[:,:,k], x_ref, u_ref, x_opt[:,0, k+1],
-                                u_opt[:,:,k], l[k], descent_arm[k], visu_descent_plot)
+            gamma = select_stepsize( 1, 20,  0.5, 0.7, del_u[:,:,k], dJ, x_ref, u_ref, x_opt[:,:, k],
+                                u_opt[:,:,k], l[k], K_star[:,:,:,k], sigma_star[:,:,k], k, visu_descent_plot)
             print('gamma:',gamma)
  
         else:
